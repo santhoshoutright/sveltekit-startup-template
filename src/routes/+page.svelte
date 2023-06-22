a3678e3553966482230e5d2b817f100b943e9b8a
Santhosh <santhsohoutright@gmail.com>
2023-06-22 22:00:32 +0530

    Setup done

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<div class="p-10 w-full">
+	<h1 class="bg-primary text-white p-4 rounded text-3xl font-bold underline">Welcome to BigBlockers web development</h1>
+	<p class="mb-4">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <h1 class="mb-4">Below is just daisy ui premade classes on top of TailwindCSS</h1>
+        <p class="mb-4"> Visit <a href="https://daisyui.com/components/"> Daisy UI</a>Use those classes if you find it helpful to finish this website</p>
+        <p class="mb-4"> Yes, you can start working on this to make a BigBlockers website, Congrats guys!</p>
+	<div class="space-y-2">
+		<div class="collapse collapse-plus bg-base-200">
+			<input type="radio" name="my-accordion-3" checked="checked" />
+			<div class="collapse-title text-xl font-medium">Click to open this one and close others</div>
+			<div class="collapse-content">
+				<p>hello</p>
+			</div>
+		</div>
+		<div class="collapse collapse-plus bg-base-200">
+			<input type="radio" name="my-accordion-3" />
+			<div class="collapse-title text-xl font-medium">Click to open this one and close others</div>
+			<div class="collapse-content">
+				<p>hello</p>
+			</div>
+		</div>
+		<div class="collapse collapse-plus bg-base-200">
+			<input type="radio" name="my-accordion-3" />
+			<div class="collapse-title text-xl font-medium">Click to open this one and close others</div>
+			<div class="collapse-content">
+				<p>hello</p>
+			</div>
+		</div>
+	</div>
+</div>
